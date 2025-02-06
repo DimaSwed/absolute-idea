9f161ceb1e5cbe7b82131dd28788154ef93e5e06
Dmitry Bulankin <swed88@mail.ru>
2025-02-06 14:39:44 +0300

    i18n fix

--- i18n.ts
+++ i18n.ts
@@ -3,15 +3,15 @@ import { initReactI18next } from 'react-i18next'
 
 i18n.use(initReactI18next).init({
   resources: {
-    eng: {
-      translation: require('./public/locales/eng/common.json')
+    en: {
+      translation: require('./public/locales/en/common.json')
     },
     cs: {
       translation: require('./public/locales/cs/common.json')
     }
   },
-  lng: 'eng',
-  fallbackLng: 'eng',
+  lng: 'en',
+  fallbackLng: 'en',
   interpolation: {
     escapeValue: false
   }
