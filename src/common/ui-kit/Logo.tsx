1f19c428f47cd31b134560ab4d5806c92f86b426
Dmitry Bulankin <swed88@mail.ru>
2025-02-20 16:46:50 +0300

    fix logo

--- src/common/ui-kit/Logo.tsx
+++ src/common/ui-kit/Logo.tsx
@@ -1,20 +1,34 @@
 import { FC } from 'react'
-import { Box } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 
 const Logo: FC = () => {
   return (
-    <Box
-      component="img"
-      sx={{
-        height: '40px',
-        width: '182px',
-        cursor: 'pointer',
-        '@media (max-width:480px)': { height: '40px', width: '182px' },
-        '@media (max-width:320px)': { height: '33px', width: '150px' }
-      }}
-      alt="Logo"
-      src="./brand.svg"
-    />
+    <Stack direction="row" alignItems={'center'} gap={1}>
+      <Box
+        component="img"
+        sx={{
+          // height: '40px',
+          // width: '182px',
+          cursor: 'pointer',
+          '@media (max-width:480px)': { height: '40px', width: '182px' },
+          '@media (max-width:320px)': { height: '33px', width: '150px' }
+        }}
+        alt="Logo"
+        // src="./brand.svg"
+        src="./brand2.png"
+      />
+      <Typography
+        sx={{
+          textAlign: 'center',
+          textDecoration: 'none',
+          color: '#fff',
+          fontWeight: 800,
+          mt: '5px'
+        }}
+      >
+        Absolute Ideas
+      </Typography>
+    </Stack>
   )
 }
 
